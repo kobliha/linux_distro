1451896de25994262e86ea812c616574414eeda2
Lukas Ocilka <lukas.ocilka@gmail.com>
2012-03-12 11:14:39 +0100

    Simplified testsuite
    - Less confusion when using constants from the library itself

--- test/test_linux_distro.rb
+++ test/test_linux_distro.rb
@@ -12,16 +12,18 @@ class LinuxDistroTest < Test::Unit::TestCase
 
   ### HELPER METHODS ###
 
-  FILE_METHOD_DISTROS = {
-    :fedora             => LinuxDistro::D_FEDORA,
-    :suse               => LinuxDistro::D_SUSE,
-    :centos             => LinuxDistro::D_CENTOS,
-    :mandriva           => LinuxDistro::D_MANDRIVA,
-  }
+  FILE_METHOD_DISTROS = [
+    LinuxDistro::D_FEDORA,
+    LinuxDistro::D_SUSE,
+    LinuxDistro::D_CENTOS,
+    LinuxDistro::D_MANDRIVA,
+  ]
+
+  ALL_DISTROS = FILE_METHOD_DISTROS
 
   def set_distro (distro)
-    unless FILE_METHOD_DISTROS[distro].nil?
-      distro_file = LinuxDistro::DISTRO_FILES[FILE_METHOD_DISTROS[distro]]
+    if FILE_METHOD_DISTROS.include?(distro)
+      distro_file = LinuxDistro::DISTRO_FILES[distro]
       File.stubs(:exists?).with(anything).returns(false)
       File.stubs(:exists?).with(distro_file).returns(true)
       # setup is finished
@@ -35,31 +37,31 @@ class LinuxDistroTest < Test::Unit::TestCase
   ### TESTS ###
 
   def test_fedora?
-    set_distro :fedora
+    set_distro LinuxDistro::D_FEDORA
 
     assert LinuxDistro::fedora?, "Should return true: #{LinuxDistro::fedora?}, Distro is: #{LinuxDistro::distro}"
   end
 
   def test_suse?
-    set_distro :suse
+    set_distro LinuxDistro::D_SUSE
 
     assert LinuxDistro::suse?, "Should return true: #{LinuxDistro::suse?}, Distro is: #{LinuxDistro::distro}"
   end
 
   def test_centos?
-    set_distro :centos
+    set_distro LinuxDistro::D_CENTOS
 
     assert LinuxDistro::centos?, "Should return true: #{LinuxDistro::centos?}, Distro is: #{LinuxDistro::distro}"
   end
 
   def test_mandriva?
-    set_distro :mandriva
+    set_distro LinuxDistro::D_MANDRIVA
 
     assert LinuxDistro::mandriva?, "Should return true: #{LinuxDistro::mandriva?}, Distro is: #{LinuxDistro::distro}"
   end
 
   def test_distro
-    [:fedora, :suse, :centos, :mandriva].each do |distro|
+    ALL_DISTROS.each do |distro|
       set_distro distro
       assert_equal distro, LinuxDistro::distro, "Should return #{distro}, Distro is: #{LinuxDistro::distro}"
       # Needed for LinuxDistro to reinitialize
